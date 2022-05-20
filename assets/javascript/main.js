840e197dcb1e143f58f6cde7340ed4dd1a02ed5b
quyendv <daovanquyen45@gmail.com>
2022-05-21 02:52:09 +0700

    fix login, register + footer

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,12 @@
 // modal login parameters
-const loginModal = document.querySelector(".js-login");
-const registerModal = document.querySelector(".js-register");
-const loginBtns = document.querySelectorAll(".js-login-btn");
-const registerBtn = document.querySelector(".js-register-btn");
+// const loginModal = document.querySelector(".js-login");
+// const registerModal = document.querySelector(".js-register");
+// const loginBtns = document.querySelectorAll(".js-login-btn");
+// const registerBtn = document.querySelector(".js-register-btn");
 
-const modalCloseBtns = document.querySelectorAll(".js-modal__close-btn");
-const modals = document.querySelectorAll(".js-modal");
-const modalContainers = document.querySelectorAll(".js-modal__container");
+// const modalCloseBtns = document.querySelectorAll(".js-modal__close-btn");
+// const modals = document.querySelectorAll(".js-modal");
+// const modalContainers = document.querySelectorAll(".js-modal__container");
 
 // mobileMenu parameters
 const mobileMenu = document.querySelector('#header #mobile-menu');
@@ -27,40 +27,41 @@ for (const nav_item of nav_items) {
 mobileMenu.addEventListener('click', toggleMobileMenu)
 
 // show loginModal
-for (const loginBtn of loginBtns) {
-    loginBtn.addEventListener("click", () => {
-        // if (mobileMenu.classList.contains('active')) toggleMobileMenu()  // hide nav_bar
+// for (const loginBtn of loginBtns) {
+//     loginBtn.addEventListener("click", () => {
+//         // if (mobileMenu.classList.contains('active')) toggleMobileMenu()  // hide nav_bar
 
-        loginModal.classList.add("open");
-        registerModal.classList.remove("open");
-    });
-}
+//         loginModal.classList.add("open");
+//         registerModal.classList.remove("open");
+//     });
+// }
+
 // show registerModal
-registerBtn.addEventListener("click", () => {
-    // if (mobileMenu.classList.contains('active')) toggleMobileMenu()  // hide nav_bar
+// registerBtn.addEventListener("click", () => {
+//     // if (mobileMenu.classList.contains('active')) toggleMobileMenu()  // hide nav_bar
 
-    loginModal.classList.remove("open");
-    registerModal.classList.add("open");
-})
+//     loginModal.classList.remove("open");
+//     registerModal.classList.add("open");
+// })
 
 // hideModal
-function hideModal() {
-    loginModal.classList.remove("open");
-    registerModal.classList.remove("open");
-}
+// function hideModal() {
+//     loginModal.classList.remove("open");
+//     registerModal.classList.remove("open");
+// }
 
-for (const closeBtn of modalCloseBtns) {
-    closeBtn.addEventListener("click", hideModal);
-}
+// for (const closeBtn of modalCloseBtns) {
+//     closeBtn.addEventListener("click", hideModal);
+// }
 
-for (const modal of modals) {
-    modal.addEventListener("click", hideModal);
-}
+// for (const modal of modals) {
+//     modal.addEventListener("click", hideModal);
+// }
 
-for (const modalContainer of modalContainers) {
-    modalContainer.addEventListener("click", function (event) {
-        event.stopPropagation();
-    });
-}
+// for (const modalContainer of modalContainers) {
+//     modalContainer.addEventListener("click", function (event) {
+//         event.stopPropagation();
+//     });
+// }
 
 // header - navigation - mobile menu
