7c49a4a286a87696b53e6a2f04d30402ce53ad90
quyendv <daovanquyen45@gmail.com>
2022-05-22 22:34:44 +0700

    booking

--- assets/javascript/booking.js
+++ assets/javascript/booking.js
@@ -1,11 +1,14 @@
 // booking parameters
-const containerBooking = document.querySelector("#booking .container");
-const seats = document.querySelectorAll("#booking .row-seat .seat:not(.occupied)");
-const countSelectedSeat = document.querySelector("#booking .count");
-const totalPrice = document.querySelector("#booking .total");
+const seatsAndScreen = document.querySelector("#booking .seats-screen"); /* Nên dùng list các row-seat hơn, tuy nhiên thêm vòng lặp */
+const seats = document.querySelectorAll("#booking .row-seat .seat:not(.occupied)"); /* seat can toggle */
+const countSelectedSeat = document.querySelector("#booking .seats-number");
+const countPremiumSelectedSeat = document.querySelector('#booking .seats-premium-number')
+const totalPrice = document.querySelector("#booking .total-price");
+
+
 // const ticketTypes = document.querySelector("#booking #ticket-types");
-const regularTicketPrice = 2;
-const premiumTicketPrice = 5;
+const regularTicketPrice = 120000;
+const premiumTicketPrice = 150000;
 
 // let ticketPrice = +ticketTypes.value;
 
@@ -15,17 +18,22 @@ function updateSelectedCount() {
   const premiumSelectedSeats = document.querySelectorAll(".row-seat .seat.selected.premium");
 
   countSelectedSeat.innerHTML = regularSelectedSeats.length + premiumSelectedSeats.length;
-  totalPrice.innerHTML = +regularSelectedSeats.length * regularTicketPrice + +premiumSelectedSeats.length * premiumTicketPrice;
+  countPremiumSelectedSeat.innerHTML = premiumSelectedSeats.length;
+  totalPrice.innerHTML = (+regularSelectedSeats.length * regularTicketPrice + +premiumSelectedSeats.length * premiumTicketPrice).toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
 
 // seats.addEventListener("change", (e) => {
 //   console.log(e.target.classList);
 // });
 
-containerBooking.addEventListener("click", (e) => {
+seatsAndScreen.addEventListener("click", (e) => {
   if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
     e.target.classList.toggle("selected");
   }
 
   updateSelectedCount();
 });
+
+
+var today = new Date().toISOString().split('T')[0];
+document.querySelector('.booking-form-input #date-booking').setAttribute('min', today);
